5d97ab19371382f47e6646c816eb76f50541ce9d
Lova Andriarimalala <43842786+Xpirix@users.noreply.github.com>
2024-11-18 11:33:19 +0300

    Remove rejected organizations list (#1472)
    * Remove rejected organizations list
    
    * Fix lint error in workflow

--- django_project/certification/urls.py
+++ django_project/certification/urls.py
@@ -15,7 +15,6 @@ from .views import (
     CertifyingOrganisationJson,
     ApproveCertifyingOrganisationView,
     reject_certifying_organisation,
-    RejectedCertifyingOrganisationListView,
 
     # Course Type.
     CourseTypeCreateView,
@@ -129,10 +128,6 @@ urlpatterns = [
               '(?P<slug>[\w-]+)/$',
         view=UpdateStatusOrganisation.as_view(),
         name='certifyingorganisation-update-status'),
-    url(regex='^(?P<project_slug>[\w-]+)/'
-              'certifyingorganisation/rejected-list/$',
-        view=RejectedCertifyingOrganisationListView.as_view(),
-        name='certifyingorganisation-rejected-list'),
     url(regex='^(?P<project_slug>[\w-]+)/certifyingorganisation/list/$',
         view=CertifyingOrganisationListView.as_view(),
         name='certifyingorganisation-list'),
