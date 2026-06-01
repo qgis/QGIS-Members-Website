a48972c8a5d901077350d507fca4cd40375721ae
Xpirix <xpirixii@gmail.com>
2026-06-01 15:38:59 +0300

    Add error handling for OSError in Sentry integration and update uwsgi config

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -4,11 +4,12 @@ core.settings.base
 """
 # Django settings for projecta project.
 import os
+
 from .utils import absolute_path
 
 ADMINS = (
-    ('Tim Sutton', 'tim@kartoza.com'),
-    ('Dimas Ciputra', 'dimas@kartoza.com'),
+    ("Tim Sutton", "tim@kartoza.com"),
+    ("Dimas Ciputra", "dimas@kartoza.com"),
 )
 
 MANAGERS = ADMINS
@@ -17,11 +18,11 @@ MANAGERS = ADMINS
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Africa/Johannesburg'
+TIME_ZONE = "Africa/Johannesburg"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
 SITE_ID = 1
 
@@ -38,24 +39,24 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/web/media'
+MEDIA_ROOT = "/home/web/media"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
 # MEDIA_URL = '/media/'
 # setting full MEDIA_URL to be able to use it for the feeds
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/web/static'
+STATIC_ROOT = "/home/web/static"
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -65,16 +66,16 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    absolute_path('core', 'base_static'),
-    absolute_path('changes', 'static'),
+    absolute_path("core", "base_static"),
+    absolute_path("changes", "static"),
 )
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'pipeline.finders.PipelineFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "pipeline.finders.PipelineFinder",
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
@@ -84,72 +85,71 @@ from .secret import SECRET_KEY  # noqa
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
             # project level templates
-            absolute_path('core', 'base_templates'),
-            absolute_path('base', 'templates'),
-            absolute_path('vota', 'templates'),
-            absolute_path('changes', 'templates'),
-            absolute_path('certification', 'templates'),
-            absolute_path('lesson', 'templates'),
+            absolute_path("core", "base_templates"),
+            absolute_path("base", "templates"),
+            absolute_path("vota", "templates"),
+            absolute_path("changes", "templates"),
+            absolute_path("certification", "templates"),
+            absolute_path("lesson", "templates"),
         ],
-        'APP_DIRS': False,
-        'OPTIONS': {
+        "APP_DIRS": False,
+        "OPTIONS": {
             # List of callables that know how to import templates from various
             # sources.
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
+            "loaders": [
+                "django.template.loaders.filesystem.Loader",
+                "django.template.loaders.app_directories.Loader",
             ],
-            'context_processors': [
+            "context_processors": [
                 # Already defined Django-related contexts
-                'django.contrib.auth.context_processors.auth',
-                'core.context_processors.add_intercom_app_id',
-                'core.context_processors.stripe_public_key',
-                'core.context_processors.sustaining_member_context',
-                'django.template.context_processors.i18n',
-
+                "django.contrib.auth.context_processors.auth",
+                "core.context_processors.add_intercom_app_id",
+                "core.context_processors.stripe_public_key",
+                "core.context_processors.sustaining_member_context",
+                "django.template.context_processors.i18n",
                 # `allauth` needs this from django
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # For custom domain checking
-    'core.custom_middleware.CheckDomainMiddleware',
-    'simple_history.middleware.HistoryRequestMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
+    "core.custom_middleware.CheckDomainMiddleware",
+    "simple_history.middleware.HistoryRequestMiddleware",
+    "django.contrib.flatpages.middleware.FlatpageFallbackMiddleware",
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sites',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'django.contrib.syndication',
-    'django.contrib.gis',
-    'django.contrib.flatpages',
-    'webpack_loader',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sites",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.admin",
+    "django.contrib.syndication",
+    "django.contrib.gis",
+    "django.contrib.flatpages",
+    "webpack_loader",
 ]
 
 # A sample logging configuration. The only tangible logging
@@ -158,36 +158,32 @@ INSTALLED_APPS = [
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
         },
-        'null': {
-            'class': 'logging.NullHandler',
+        "null": {
+            "class": "logging.NullHandler",
         },
     },
-    'loggers': {
+    "loggers": {
         # Special rules to not bother logging when host is
         # not allowed otherwise we get lots of mail spam....
-        'django.security.DisallowedHost': {
-            'handlers': ['null'],
-            'propagate': False,
+        "django.security.DisallowedHost": {
+            "handlers": ["null"],
+            "propagate": False,
         },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
         },
-    }
+    },
 }
 
 # Sentry
@@ -196,6 +192,13 @@ SENTRY_RATE = os.environ.get("SENTRY_RATE", 1.0)
 
 if SENTRY_DSN and SENTRY_DSN != "":
     import sentry_sdk
+    from sentry_sdk.types import Event, Hint
+
+    def before_send(event: Event, hint: Hint):
+        exc = hint.get("exc_info", (None,))[1]
+        if isinstance(exc, OSError) and "write error" in str(exc):
+            return None  # Drop the event
+        return event
 
     sentry_sdk.init(
         dsn=SENTRY_DSN,
@@ -204,14 +207,14 @@ if SENTRY_DSN and SENTRY_DSN != "":
         traces_sample_rate=SENTRY_RATE,
     )
 
-STRIPE_API_VERSION = '2019-09-09'
+STRIPE_API_VERSION = "2019-09-09"
 
 SITE_ROOT = os.path.dirname(BASE_DIR)
 
 # Webpack
 WEBPACK_LOADER = {
-    'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'bundles',
-        'STATS_FILE': os.path.join(SITE_ROOT, 'webpack-stats.json'),
+    "DEFAULT": {
+        "BUNDLE_DIR_NAME": "bundles",
+        "STATS_FILE": os.path.join(SITE_ROOT, "webpack-stats.json"),
     }
 }
